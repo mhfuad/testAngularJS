678c57811e83f1c074bbaaefe2463adb5b8be51b
fuad <mhfuad2@gmail.com>
2017-09-15 11:40:53 +0600

    number 10

--- 10script.js
+++ 10script.js
@@ -9,4 +9,18 @@ var myApp = angular
 						{name:"Laskar",age:new Date("Feb 10, 1970"),gender:"male",salary:40000},
 					];
 					$scope.employees = employees;
+					$scope.sortColumn = "name";
+					$scope.reverseSort = false;
+
+					$scope.sortData = function (column) {
+						$scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+						$scope.sortColumn = column;
+					}
+
+					$scope.getSortClass = function (column) {
+						if($scope.sortColumn == column){
+							return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+						}
+						return '';
+					}
 				});
