21c99ac2a6578226797650c86fb90fd3f49f3c78
fuad <mhfuad2@gmail.com>
2017-09-27 19:43:18 +0600

    21 done

--- 21script.js
+++ 21script.js
@@ -1,5 +1,8 @@
 var app = angular
 			.module('myModule',[])
-			.controller('myController',function($scope,stringService){
-				
+			.controller('myController',function($scope,$location,$anchorScroll){
+				$scope.scrollTo = function(scrollLocation){
+					$location.hash(scrollLocation);
+					$anchorScroll();
+				}
 			});
